102145770d5922ce624be255fa1a69abe8637539
Florian Kühlkamp <florian.kuehlkamp@hslu.ch>
2024-11-29 11:27:53 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class QueryHandler:
+    def __init__(self) -> None:
+        self.query_result:str = None
+
+    def _process_query(self, query:str) -> str:
+        return query
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
